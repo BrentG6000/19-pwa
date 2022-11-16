41dae9c3fdea77240b780bb7bfaf90275c205ded
Brent Gustafson <50030696+BrentG6000@users.noreply.github.com>
2022-11-15 23:43:12 -0600

    Added code to src-sw and webpack.config.js

---
+++ main/client/src/js/database.js
@@ -0,0 +1,27 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  const contactDb = await openDB('jate', 1);
+  const tx = contactDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.add({ name: name, home_phone: home, cell_phone: cell, email: email });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+}
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => console.error('getDb not implemented');
+
+initdb();
