e69222e5a4ac10a0e172f800265ab05f7af1ef4d
Brent Gustafson <50030696+BrentG6000@users.noreply.github.com>
2022-11-25 21:39:06 -0600

    Added README.md

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# 19-pwa
+# 19-PWA
+
+## Description
+
+The motivation for creating this web app was to create a progressive web app that simulates a terminal text editor. Using this app you can practice typing into a bash terminal without having to install bash to your device. While creating this app I learned how to successfully deploy an app progressively so that it runs without the need for an internet connection.
+
+## Installation
+
+To install this app, you can either download the code from this repository or you can visit the live web site at https://protected-river-27350.herokuapp.com/. If you choose to download the code,
+go to the root downloaded folder and run "npm i" in your terminal to install all node modules. Run "npm run start" to begin the program which will open your default browser with the app running.
+
+## Usage
+
+To use this app you can simply start typing in the text area. To install this app to your device click the install button. Your browser may ask you if you want to install the app. Answer yes and the app will
+install to your system. The app will be available on your desktop even when there is no internet connection.
+
+
+    ![screenshot](/public/assets/images/screenshot.png)
+
+## License
+
+MIT License
+
+---
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,44 +12,41 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  // First, create a variable, and set it to asyncronously await the opening of the database. Replace the items in all caps
+  // Create a variable and set it to asyncronously await the opening of the database. Replace the items in all caps
   const DB_VAR = await openDB('jate', 1);
 
-  // Now create a variable for the transaction
+  // Create a variable for the transaction
   const TX_VAR = DB_VAR.transaction('jate', 'readwrite');
 
-  // Now create a variable for the store
+  // Create a variable for the store
   const STORE_VAR = TX_VAR.objectStore('jate');
 
-  // Now create a variable named "request" and have it perform the update
+  // Create a variable named "request" and have it perform the update
   const request = STORE_VAR.put({ id: 1, value: content });
 
   const result = await request;
   console.log('🚀 - data saved to the database', result.value);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  // You can duplicate the same first lines of code from above, except that the transaction will be 'readonly'
   
-  // LINES 1-3 HERE
   const DB_VAR = await openDB('jate', 1);
 
-  // Now create a variable for the transaction
+  // Create a variable for the transaction
   const TX_VAR = DB_VAR.transaction('jate', 'readonly');
 
-  // Now create a variable for the store
+  // Create a variable for the store
   const STORE_VAR = TX_VAR.objectStore('jate');
 
-  // Leave the rest as-is
   const request = STORE_VAR.get(1);
   const result = await request;
   result
     ? console.log('🚀 - data retrieved from the database', result.value)
     : console.log('🚀 - data not found in the database');
-  // Check if a variable is defined and if it is, return it. See MDN Docs on Optional Chaining (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining)
+  // Check if a variable is defined and if it is, return it.
   return result?.value;
 };
 
